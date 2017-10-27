db063aa55adc445e79b0b50fa5a4903b9fa7d008
Vineeth <vineeth.madhu@gmail.com>
2017-10-27 15:51:34 +0530

    Applied Google Coding Style

--- JsonParserMain/code/java/jsonp/Constants.java
+++ JsonParserMain/code/java/jsonp/Constants.java
@@ -1,6 +1,7 @@
 package code.java.jsonp;
 
 public class Constants {
-	public static final String NODE_DELIMITER = ",";
-	public static final String FIELD_DELIMITER = "\\.";
+
+  public static final String NODE_DELIMITER = ",";
+  public static final String FIELD_DELIMITER = "\\.";
 }
