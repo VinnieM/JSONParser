6bd9359dcd727dbf639908bb493d84af160e5d28
Menon <MENOV27@TDBFG.COM>
2018-09-19 01:53:18 -0400

    Added new Set method and added comments to the code

--- JsonParserMain/code/java/jsonp/JSONP.java
+++ JsonParserMain/code/java/jsonp/JSONP.java
@@ -6,6 +6,13 @@ import org.json.JSONObject;
 
 public class JSONP {
 
+  /**
+   * This function get's the node value of the JSON Object/Array.
+   *
+   * @param inputJson The JSON Structure from which the value needs to be obtained
+   * @param objectKey The key for which value is required
+   * @return A Java Object which contains the value.
+   */
   public Object getNodeValue(JSONObject inputJson, String objectKey) {
       Object currentObject = inputJson;
       String[] fields = objectKey.split(Constants.FIELD_DELIMITER);
@@ -25,6 +32,57 @@ public class JSONP {
         }
       }
       return currentObject;
+  }
+
+  /**
+   * This function is used to set a value to for a given Key. If the Key is not present a
+   * new Key is added. A JSON structure with the changed value is returned.
+   *
+   * @param inputJson The JSON which needs to be modified.
+   * @param objectKey The Key which needs to be found.
+   * @param value The new value which needs to be set.
+   * @see <a href="">Illegal State Exception</a>
+   * @see <a href="">Illegal Argument Exception</a>
+   */
+  public JSONObject setNodeValue(JSONObject inputJson, String objectKey, String value) {
+    Object currentObject = inputJson;
+    String[] fields = objectKey.split(Constants.FIELD_DELIMITER);
+    for (int i = 0; i < fields.length; i++) {
+      if (i == fields.length - 1) {
+        if (JSONObject.class.isInstance(currentObject)) {
+          setJSONObject(currentObject, fields[i], value);
+          return inputJson;
+        } else {
+          throw new IllegalStateException("Only JSON Objects can be modified!!!");
+        }
+      }
+      if (fields[i].contains("\n")) {
+        fields[i] = fields[i].substring(0, fields[i].length() - 1);
+        if (fields[i].isEmpty()) {
+          throw new IllegalStateException();
+        }
+      }
+      if (JSONObject.class.isInstance(currentObject)) {
+        currentObject = getFromJSONObject(currentObject, fields[i]);
+      } else if (JSONArray.class.isInstance(currentObject)) {
+        currentObject = getFromJSONArray(currentObject, fields[i]);
+      } else {
+        throw new IllegalArgumentException();
+      }
+    }
+    throw new IllegalArgumentException("Key cannot be empty!!!");
+  }
+
+  private Object setJSONObject(Object inputObj, String key, String value) {
+    try {
+      if (((JSONObject) inputObj).has(key)) {
+        return ((JSONObject) inputObj).put(key, value);
+      }
+      return ((JSONObject) inputObj).put(key, value);
+    } catch (Exception w) {
+      w.printStackTrace();
+      return w;
+    }
   }
 
   private Object getFromJSONObject(Object inputObj, String key) {
